4d189e00167ce4099a2e45e4c6429bfb1c53f4d5
Felix Bechmann <felixbechmann@yahoo.de>
2016-12-20 02:08:01 +0100

    implemented the basic log class stump and a nice comment at the top

---
+++ logWebsite/Log.py
@@ -0,0 +1,19 @@
+'''
+This class contains all the informations about a single log file from the datebase
+It must contain:
+- A message
+- The event Time at which the log was created
+- The error, which was thrown at the logging moment
+
+'''
+
+
+
+
+
+class Log:
+    #erstellen der logdateien mit
+    def __init__(self, ):
+        return
+
+
